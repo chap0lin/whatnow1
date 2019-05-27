587786315dcc0c249324f0898d9f1b192f376b4a
chap0lin <carlosrocha@mecajun.com>
2019-05-26 23:47:18 -0300

    novo

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React!</Text>
       </View>
     );
   }
